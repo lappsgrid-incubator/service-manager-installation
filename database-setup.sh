c39bb6cd952202aed01de2396a88894327098656
Keith Suderman <suderman@cs.vassar.edu>
2017-02-17 15:00:39 -0500

    The /tmp/*.sql files are removed in setup.sh since the postgres user doesn't have rights.

--- database-setup.sh
+++ database-setup.sh
@@ -1,10 +1,9 @@
 createuser -S -D -R $ROLENAME
 psql --command "ALTER USER $ROLENAME WITH PASSWORD '$PASSWORD'"
 createdb $DATABASE -O $ROLENAME -E 'UTF8'
-createlang plpgsql $DATABASE
+#createlang plpgsql $DATABASE
 psql $DATABASE < /tmp/create_indices.sql
 psql $DATABASE < /tmp/create_storedproc.sql
 echo "ALTER FUNCTION \"AccessStat.increment\"(character varying, character varying, character varying, character varying, character varying, timestamp without time zone, timestamp without time zone, integer, timestamp without time zone, integer, timestamp without time zone, integer, integer, integer, integer) OWNER TO $ROLENAME" > /tmp/alter.sql
 psql $DATABSE < /tmp/alter.sql
-rm /tmp/*.sql
 #psql $DATABASE -c "ALTER FUNCTION \"AccessStat.increment\"(character varying, character varying, character varying, character varying, character varying, timestamp without time zone, timestamp without time zone, integer, timestamp without time zone, integer, timestamp without time zone, integer, integer, integer, integer) OWNER TO $ROLENAME"
