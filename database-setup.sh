ebe1841e49ae3940fd5e43be4587ac95eb539f32
Keith Suderman <suderman@cs.vassar.edu>
2017-02-17 12:01:47 -0500

    Cleans up .sql files in /tmp

--- database-setup.sh
+++ database-setup.sh
@@ -6,4 +6,5 @@ psql $DATABASE < /tmp/create_indices.sql
 psql $DATABASE < /tmp/create_storedproc.sql
 echo "ALTER FUNCTION \"AccessStat.increment\"(character varying, character varying, character varying, character varying, character varying, timestamp without time zone, timestamp without time zone, integer, timestamp without time zone, integer, timestamp without time zone, integer, integer, integer, integer) OWNER TO $ROLENAME" > /tmp/alter.sql
 psql $DATABSE < /tmp/alter.sql
+rm /tmp/*.sql
 #psql $DATABASE -c "ALTER FUNCTION \"AccessStat.increment\"(character varying, character varying, character varying, character varying, character varying, timestamp without time zone, timestamp without time zone, integer, timestamp without time zone, integer, timestamp without time zone, integer, integer, integer, integer) OWNER TO $ROLENAME"
