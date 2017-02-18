f8e956551cc65068823033e57d7f06c9ffdc58f0
Keith Suderman <suderman@cs.vassar.edu>
2017-02-18 15:01:50 -0500

    CentOS fixes.

--- setup.sh
+++ setup.sh
@@ -5,6 +5,10 @@ smg=smg-1.1.0-SNAPSHOT
 manager=http://downloads.lappsgrid.org/service-manager
 scripts=http://downloads.lappsgrid.org/scripts
 
+# Locations that Tomcat is installed.
+MANAGER=/usr/share/tomcat/service-manager
+BPEL=/usr/share/tomcat/active-bpel
+
 function usage()
 {
 	echo
@@ -16,6 +20,36 @@ function usage()
 
 function log {
 	echo $1 | tee /var/log/service-manager-install.log >&2
+}
+
+function start_tomcat {
+	if [[ $OS = redhat7 || $OS = centos]] ; then
+		systemctl start tomcat
+	else
+		service tomcat start
+	fi
+}
+
+function stop_tomcat {
+	if [[ $OS = redhat7 || $OS = centos]] ; then
+		systemctl stop tomcat
+	else
+		service tomcat stop
+	fi
+}
+
+function wait_for {
+	while ! grep "Server startup in" $1/log/catalina.out ; do
+		echo "Waiting for $1 to start"
+		sleep 1
+	done
+}
+
+function toggle_tomcat {
+	start_tomcat
+	wait_for $MANAGER
+	wait_for $BPEL
+	stop_tomcat
 }
 
 source <(curl -sSL http://downloads.lappsgrid.org/scripts/sniff.sh)
@@ -58,6 +92,15 @@ curl -sSL $scripts/install-java.sh | bash
 log "Installing PostgreSQL"
 curl -sSL $scripts/install-postgres.sh | bash
 
+if [[ $OS = centos || $OS = redhat7 ]] ; then
+	hba=/var/lib/pgsql/9.6/data/pg_hba.conf
+	rm $hba
+	echo "local all all trust" > $hba
+	echo "host all all 127.0.0.1/32 trust" >> $hba
+	echo "host all all ::1/128 trust" >> $hba
+	systemctl restart postgresql-9.6
+fi
+
 if [ ! -e ServiceManager.config ] ; then
 	wget $manager/ServiceManager.config
 fi
@@ -76,11 +119,14 @@ chmod +x $smg/smg
 # 	langrid.ae.properties
 # 	db.config
 $smg/smg ServiceManager.config
+source db.config
+
+sudo -u postgres createuser -S -D -R $ROLENAME
+sudo -u postgres psql --command "ALTER USER $ROLENAME WITH PASSWORD '$PASSWORD'"
+sudo -u postgres createdb $DATABASE -O $ROLENAME -E 'UTF8'
 
 # Now install Tomcat and create the PostgreSQL database.
 log "Starting Tomcat installation."
-MANAGER=/usr/share/tomcat/service-manager
-BPEL=/usr/share/tomcat/active-bpel
 curl -sSL $manager/install-tomcat.sh | bash
 
 cp tomcat-users.xml $MANAGER/conf
@@ -90,19 +136,30 @@ cp tomcat-users-bpel.xml $BPEL/conf/tomcat-users.xml
 cp active-bpel.xml $BPEL/conf/Catalina/localhost
 cp langrid.ae.properties $BPEL/bpr
 
-log "Creating role, database and stored procedure."
+toggle_tomcat
+
+log "Creating indices and stored procedure."
 # Grab the SQL and setup scripts.
 wget $manager/create_storedproc.sql
 wget $manager/create_indices.sql
 wget $manager/database-setup.sh
 
 # Generate a custom database-setup.sh with the settings generated above.
-echo '#!/usr/bin/env bash' | cat - db.config database-setup.sh > /tmp/database-setup.sh
-chmod +x /tmp/database-setup.sh
-mv *.sql /tmp
+#echo '#!/usr/bin/env bash' | cat - db.config database-setup.sh > /tmp/database-setup.sh
+#chmod +x /tmp/database-setup.sh
+#mv *.sql /tmp
 # Now run the setup script as the postgres user.
-su - postgres -c "bash /tmp/database-setup.sh"
-rm /tmp/*.sql
+#su - postgres -c "bash /tmp/database-setup.sh"
+#rm /tmp/*.sql
+
+#createlang plpgsql $DATABASE
+sudo -u postgres psql $DATABASE < /tmp/create_indices.sql
+sudo -u postgres psql $DATABASE < /tmp/create_storedproc.sql
+echo "ALTER FUNCTION \"AccessStat.increment\"(character varying, character varying, character varying, character varying, character varying, timestamp without time zone, timestamp without time zone, integer, timestamp without time zone, integer, timestamp without time zone, integer, integer, integer, integer) OWNER TO $ROLENAME" > /tmp/alter.sql
+#psql $DATABSE < /tmp/alter.sql
+cat /tmp/alter.sql | sudo -u postgres psql $DATABASE
+
+#psql $DATABASE -c "ALTER FUNCTION \"AccessStat.increment\"(character varying, character varying, character varying, character varying, character varying, timestamp without time zone, timestamp without time zone, integer, timestamp without time zone, integer, timestamp without time zone, integer, integer, integer, integer) OWNER TO $ROLENAME"
 
 log "Securing the Tomcat installations"
 for dir in $MANAGER $BPEL ; do
@@ -131,11 +188,13 @@ for dir in $MANAGER/webapps $BPEL/webapps ; do
 	popd > /dev/null
 done
 
-if [[ $OS = redhat7 ]] ; then
-	systemctl start tomcat
-else
-	service tomcat start
-fi
+#if [[ $OS = redhat7 ]] ; then
+#	systemctl start tomcat
+#else
+#	service tomcat start
+#fi
+
+start_tomcat 
 
 log "The Service Grid is now running."
 echo
