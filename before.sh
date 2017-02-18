22eb54197fdab702fcc3685b5c68f2bc312b1ceb
Keith Suderman <suderman@cs.vassar.edu>
2017-02-18 13:42:08 -0500

    before.sh now creates langrid databasse before installing tomcat.

--- before.sh
+++ before.sh
@@ -58,6 +58,8 @@ curl -sSL $scripts/install-java.sh | bash
 log "Installing PostgreSQL"
 curl -sSL $scripts/install-postgres.sh | bash
 
+# Create the database and role needed by the service manager.
+
 if [ ! -e ServiceManager.config ] ; then
 	wget $manager/ServiceManager.config
 fi
@@ -77,6 +79,19 @@ chmod +x $smg/smg
 # 	db.config
 $smg/smg ServiceManager.config
 
+#wget $manager/database-setup.sh
+
+# Generate a custom database-setup.sh with the settings generated above.
+#echo '#!/usr/bin/env bash' | cat - db.config database-setup.sh > /tmp/database-setup.sh
+#chmod +x /tmp/database-setup.sh
+#mv *.sql /tmp
+# Now run the setup script as the postgres user.
+#su - postgres -c "bash /tmp/database-setup.sh"
+#rm /tmp/*.sql
+createuser -U postgres -S -D -R $ROLENAME
+psql -U postgres --command "ALTER USER $ROLENAME WITH PASSWORD '$PASSWORD'"
+createdb -U postgres $DATABASE -O $ROLENAME -E 'UTF8'
+
 # Now install Tomcat and create the PostgreSQL database.
 log "Starting Tomcat installation."
 MANAGER=/usr/share/tomcat/service-manager
@@ -90,19 +105,3 @@ cp tomcat-users-bpel.xml $BPEL/conf/tomcat-users.xml
 cp active-bpel.xml $BPEL/conf/Catalina/localhost
 cp langrid.ae.properties $BPEL/bpr
 
-log "Creating role, database and stored procedure."
-# Grab the SQL and setup scripts.
-wget $manager/create_storedproc.sql
-wget $manager/create_indices.sql
-wget $manager/database-setup.sh
-
-# Generate a custom database-setup.sh with the settings generated above.
-#echo '#!/usr/bin/env bash' | cat - db.config database-setup.sh > /tmp/database-setup.sh
-#chmod +x /tmp/database-setup.sh
-#mv *.sql /tmp
-# Now run the setup script as the postgres user.
-#su - postgres -c "bash /tmp/database-setup.sh"
-#rm /tmp/*.sql
-createuser -U postgres -S -D -R $ROLENAME
-psql -U postgres --command "ALTER USER $ROLENAME WITH PASSWORD '$PASSWORD'"
-createdb -U postgres $DATABASE -O $ROLENAME -E 'UTF8'
